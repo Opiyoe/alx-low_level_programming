556ec5a62813ff90aac6f0b050b2b131f545992d
Opiyoe <opiyoelishaa@gmail.com>
2023-07-14 13:23:49 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description-print alphabets except e and q
+ * Return:0
+ */
+int main(void)
+{
+	int w = 97;
+
+	while (w <= 122)
+	{
+		if (w == 111 || w == 113)
+			w++;
+		continue;
+		putchar(w);
+		w++;
+	}
+	putchar('\n');
+	return (0);
+}
