375c765ed9861e33eccd78e4846fc691dd6051c5
Opiyoe <opiyoelishaa@gmail.com>
2023-07-18 09:21:49 -0700

    main_head_file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,23 @@
-#include "main.h"
+include "main.h"
+:
 /**
- * print_alphabet_x10-Make alphabet x10 times
- * Return: void
+ * print_alphabet_x10 - make alphabet x10 times
+ * Return:void
  */
 void print_alphabet_x10(void)
-
 {
-
-	char c;
-	int i = 0;
-
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-
-			-putchar('\n');
-			i++;
-		}
-	}
+char c;
+int i;
+i = 0;
+while (i < 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{_putchar(c);
 }
+_putchar('\n');
+i++;
+}
+
+}
+	 
+
