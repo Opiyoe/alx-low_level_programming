12a7cfc95f84dc7c60f07755079ba5032abe5d03
Opiyoe <opiyoelishaa@gmail.com>
2023-07-20 00:41:14 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main _tests function that prints if integer is positive or negative
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive _or_negative(i);
+
+	return (0);
+}
