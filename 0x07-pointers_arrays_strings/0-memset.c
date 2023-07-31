8f153b52742c34ef1be8943548bd51e1bfcd19a4
Opiyoe <opiyoelishaa@gmail.com>
2023-07-31 06:17:00 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,11 +6,11 @@
  * @n:number of bytes
  * Return:pointer to s
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < 0; i++)
+	for (i = 0; i < n; i++)
 	s[i] = b;
 
 return (s);
